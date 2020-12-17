9eea53d082b30c6605ae8bdf4eb2c1ebe80cbb07
Irfan Darmawan <74939210+JekRed@users.noreply.github.com>
2020-12-17 12:40:13 +0700

    baru

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -35,27 +35,15 @@
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
+                      <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                         <td class="cart-pic first-row">
-                          <img src="img/cart-page/product-1.jpg" />
+                          <img class="img-cart" :src="keranjang.photo" />
                         </td>
                         <td class="cart-title first-row text-center">
-                          <h5>Pure Pineapple</h5>
+                          <h5>{{keranjang.name}}</h5>
                         </td>
-                        <td class="p-price first-row">$60.00</td>
-                        <td class="delete-item">
-                          <a href="#"><i class="material-icons"> close </i></a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="cart-pic first-row">
-                          <img src="img/cart-page/product-1.jpg" />
-                        </td>
-                        <td class="cart-title first-row text-center">
-                          <h5>Pure Pineapple</h5>
-                        </td>
-                        <td class="p-price first-row">$60.00</td>
-                        <td class="delete-item">
+                        <td class="p-price first-row">${{keranjang.price}}</td>
+                        <td @click="removeItem(keranjangUser.index)" class="delete-item">
                           <a href="#"><i class="material-icons"> close </i></a>
                         </td>
                       </tr>
@@ -75,6 +63,7 @@
                         id="namaLengkap"
                         aria-describedby="namaHelp"
                         placeholder="Masukan Nama"
+                        v-model="customerInfo.name"
                       />
                     </div>
                     <div class="form-group">
@@ -85,6 +74,7 @@
                         id="emailAddress"
                         aria-describedby="emailHelp"
                         placeholder="Masukan Email"
+                        v-model="customerInfo.email"
                       />
                     </div>
                     <div class="form-group">
@@ -95,6 +85,7 @@
                         id="noHP"
                         aria-describedby="noHPHelp"
                         placeholder="Masukan No. HP"
+                        v-model="customerInfo.number"
                       />
                     </div>
                     <div class="form-group">
@@ -103,6 +94,7 @@
                         class="form-control"
                         id="alamatLengkap"
                         rows="3"
+                        v-model="customerInfo.address"
                       ></textarea>
                     </div>
                   </form>
@@ -118,10 +110,10 @@
                     <li class="subtotal">
                       ID Transaction <span>#SH12000</span>
                     </li>
-                    <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
-                    <li class="subtotal mt-3">Pajak <span>10%</span></li>
+                    <li class="subtotal mt-3">Subtotal <span>${{totalHarga}}.00</span></li>
+                    <li class="subtotal mt-3">Pajak <span>10% ${{ditambahPajak}}.00</span></li>
                     <li class="subtotal mt-3">
-                      Total Biaya <span>$440.00</span>
+                      Total Biaya <span>${{totalBiaya}}.00</span>
                     </li>
                     <li class="subtotal mt-3">
                       Bank Transfer <span>Mandiri</span>
@@ -133,9 +125,10 @@
                       Nama Penerima <span>Shayna</span>
                     </li>
                   </ul>
-                  <router-link to="/success" class="proceed-btn"
-                    >I ALREADY PAID</router-link
-                  >
+                  <!-- <router-link to="/success"> -->
+                    <a @click="checkout()" href="#" class="proceed-btn"> ALREADY PAID </a>
+                  <!--</router-link -->
+                  
                 </div>
               </div>
             </div>
@@ -149,11 +142,80 @@
 
 <script>
 import HeaderShayna from "@/components/HeaderShayna.vue";
+import axios from "axios";
 
 export default {
   name: "cart",
   components: {
     HeaderShayna,
   },
+  data() {
+    return {
+      keranjangUser:[],
+      customerInfo:{
+        name:'',
+        email:'',
+        number:'',
+        address:''
+      }
+    };
+  },
+  methods:{
+    removeItem(index){
+      this.keranjangUser.splice(index , 1);
+      const parsed = JSON.stringify(this.keranjangUser);
+      localStorage.setItem('keranjangUser', parsed);
+    },
+    checkout(){
+      let productIds = this.keranjangUser.map(function(product){
+        return product.id
+      });
+
+      let checkoutData = {
+        'name': this.customerInfo.name,
+        'email': this.customerInfo.email,
+        'number': this.customerInfo.number,
+        'address': this.customerInfo.address,
+        "transaction_total": this.totalBiaya,
+        "transaction_status": "PENDING",
+        "transaction_details": productIds
+      };
+
+      axios
+      .post("http://shayna-deploy.jekredkoding.website/api/checkout", checkoutData)
+      .then(() => this.$router.push('success'))
+      // eslint-disable-next-line no-console
+      .catch(err => console.log(err));
+    }
+  },
+  mounted(){
+    if (localStorage.getItem('keranjangUser')) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+      } catch(e) {
+        localStorage.removeItem('keranjangUser');
+      }
+    }
+  },
+  computed:{
+    totalHarga(){
+      return this.keranjangUser.reduce(function(items, data){
+        return items + data.price;
+      }, 0);
+    },
+    ditambahPajak(){
+      return (this.totalHarga * 10) / 100;
+    },
+    totalBiaya(){
+      return this.totalHarga + this.ditambahPajak;
+    }
+  }
 };
 </script>
+
+<style scoped>
+.img-cart{
+  width: 120px;
+  height: 120px ;
+}
+</style>
